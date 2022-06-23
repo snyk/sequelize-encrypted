6c5ca5e31144c08ee2e8336f1e7e83ce1548c505
Chris West (Faux) <gpg@goeswhere.com>
2022-06-23 15:12:59 +0100

    chore: tests: use localhost for db

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import Sequelize from 'sequelize';
 import EncryptedField from '../';
 
-const dbHost = process.env.DB_HOST || 'db';
+const dbHost = process.env.DB_HOST || 'localhost';
 const sequelize = new Sequelize(`postgres://postgres@${dbHost}:5432/postgres`);
 
 const key1 = 'a593e7f567d01031d153b5af6d9a25766b95926cff91c6be3438c7f7ac37230e';
